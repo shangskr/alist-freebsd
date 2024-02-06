afa60d92a776a9b67c5829ad9ef739f3c385d7a5
安小歪 <119773750+shangskr@users.noreply.github.com>
2024-02-06 11:55:37 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-appName="synctv"
+appName="alist"
 builtAt="$(date +'%F %T %z')"
-goVersion=$(go version | awk '{print $3}')
-gitAuthor="SyncTV <pyh1670605849@gmail.com>"
+goVersion=$(go version | sed 's/go version //')
+gitAuthor="Xhofe <i@nn.ci>"
 gitCommit=$(git log --pretty=format:"%h" -1)
 version=$(git describe --long --tags --dirty --always)
-webVersion=$(wget -qO- -t1 -T2 "https://api.github.com/repos/synctv-org/synctv-web/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
+webVersion=$(wget -qO- -t1 -T2 "https://api.github.com/repos/alist-org/alist-web/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
 
 ldflags="\
 -w -s \
--X 'github.com/synctv-org/synctv/internal/conf.BuiltAt=$builtAt' \
--X 'github.com/synctv-org/synctv/internal/conf.GoVersion=$goVersion' \
--X 'github.com/synctv-org/synctv/internal/conf.GitAuthor=$gitAuthor' \
--X 'github.com/synctv-org/synctv/internal/conf.GitCommit=$gitCommit' \
--X 'github.com/synctv-org/synctv/internal/conf.Version=$version' \
--X 'github.com/synctv-org/synctv/internal/conf.WebVersion=$webVersion' \
+-X 'github.com/alist-org/alist/v3/internal/conf.BuiltAt=$builtAt' \
+-X 'github.com/alist-org/alist/v3/internal/conf.GoVersion=$goVersion' \
+-X 'github.com/alist-org/alist/v3/internal/conf.GitAuthor=$gitAuthor' \
+-X 'github.com/alist-org/alist/v3/internal/conf.GitCommit=$gitCommit' \
+-X 'github.com/alist-org/alist/v3/internal/conf.Version=$version' \
+-X 'github.com/alist-org/alist/v3/internal/conf.WebVersion=$webVersion' \
 "
 
 go build -ldflags="$ldflags" -tags=jsoniter .
